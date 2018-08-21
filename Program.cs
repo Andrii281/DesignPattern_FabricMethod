ecef6fc4b597b11cf81b4dfa60d8cb7844252ad7
Rogoda <rogoda0@gmail.com>
2018-08-21 20:22:44 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPattern_FabricMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
